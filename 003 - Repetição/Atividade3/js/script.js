17f631844d88df3923ff02b4907549efbe9aabad
JonPer3z <perez.joaopedro@gmail.com>
2025-03-07 09:58:23 -0300

    Inicio atividade 3

---
+++ 003 - Repetição/Atividade3/js/script.js
@@ -0,0 +1,39 @@
+function VerificarNumeros(){
+    const element1 = document.getElementById("value1");
+    const element2 = document.getElementById("value2");
+
+    let pares = 0;
+    let impares = 0;
+
+    if((element1 instanceof HTMLInputElement) && (element2 instanceof HTMLInputElement)){
+        const number1 = parseInt(element1.value); 
+        const number2 = parseInt(element2.value); 
+
+        if(!isNaN(number1) && !isNaN(number2)){
+            if(number1 > number2){
+                let temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            for(let i=number1; i<=number2; i++){
+                if(i % 2 === 0){
+                    pares++
+                    exibirResultado('resultadoPares',`Pares: ${pares}`);
+                }
+                else{
+                    impares++
+                    exibirResultado('resultadoImpares', `Impares: ${impares}`);
+                }
+            }
+        }
+        else{
+            alert("Erro, digite um número válido Ex. 10 ou 3");
+        }
+    }
+    else{
+        alert("Erro, tente novamente");
+    }
+}
+
+function exibirResultado() // Exibir resultado pares e impares
