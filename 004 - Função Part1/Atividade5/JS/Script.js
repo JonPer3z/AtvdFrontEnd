b7af1b045f47e0f0845bed9c9fd182b773689f54
JonPer3z <perez.joaopedro@gmail.com>
2025-05-07 11:00:03 -0300

    Atvd 5

--- 004 - Função Part1/Atividade5/JS/Script.js
+++ 004 - Função Part1/Atividade5/JS/Script.js
@@ -1,25 +1,35 @@
-function iniciar(){
+function configurar() {
     let btnVerificar = document.getElementById("verificar");
     let textInput = document.getElementById("vogais");
 
-    if((textInput instanceof HTMLInputElement) && (btnVerificar instanceof HTMLButtonElement) ){
-        console.log('bbbb') // Nao esta entrando nessa função
-        let text = String(textInput.value);
-        btnVerificar.addEventListener('click',() => {
-            console.log('aaaa')
-            contarVogais(text);
-            
+
+    if (textInput && btnVerificar) {
+        btnVerificar.addEventListener('click', () => {
+            let text = String(textInput.value);
+            let qtdVogais = contarVogais(text);
+            imprimirResultado(qtdVogais);
         });
+    } else {
+        console.error("Elementos do DOM não foram encontrados.");
     }
 }
 
 function contarVogais(text){
-    let frase = text.split('');
-    console.log(frase);
+    let frase = text.toLowerCase().split('');
     let vogais = 'aeiouáéíóúâêîôûãõàèìòù';
     let contador = 0;
-    
+
+    frase.forEach(letra => {
+        if(vogais.includes(letra)){
+            contador++;
+        }
+    });
+    return contador;
+    // Não preciso chamar a função imprimir resultado, pois ela já foi chamada na função anterior
 }
 
+function imprimirResultado(qtdVogais){
+    document.getElementById("output").innerHTML = `A frase possui ${qtdVogais} vogais`;
+}
 
-document.addEventListener('DOMContentLoaded', iniciar);
+document.addEventListener('DOMContentLoaded', configurar);
